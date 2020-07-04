cf9f3ce8366f6de0712211c646e65b9eadaa2801
hamisirawlins <53607315+hamisirawlins@users.noreply.github.com>
2020-07-04 15:53:12 +0300

    Foods list as well as description at bottom on click

---
+++ pemule/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FoodsComponent } from './foods/foods.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FoodsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
