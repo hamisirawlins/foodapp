7c7b2170a17f61b6554975753ad8f629f2aa5bd5
hamisirawlins <53607315+hamisirawlins@users.noreply.github.com>
2020-07-05 15:03:06 +0300

    Routing and Bootstrap Navbar, first dashboard

--- pemule/src/app/app-routing.module.ts
+++ pemule/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { FoodsComponent } from './foods/foods.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+const routes: Routes = [
+  { path: 'foods', component: FoodsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pemule/src/app/app.module.ts
+++ pemule/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FoodsComponent } from './foods/foods.component';
 import { FoodDetailComponent } from './food-detail/food-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodsComponent,
     FoodDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
